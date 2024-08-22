40126109257e15c88322f95dcea63e81924a3fbc
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2024-08-22 19:45:56 -0300

    :tada: First Commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call([
+            PublisherSeeder::class,
+            AuthorSeeder::class,
+            CategorySeeder::class,
+            BookSeeder::class,
+        ]);
+    }
+}
