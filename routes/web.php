a718e501d6e80afba78faf8598b5f85125c74bcf
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2024-08-28 07:03:43 -0300

    :sparkles: feat: creation of library project views.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route; // Add this line
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PublisherController;
+
+// Rotas para Books
+Route::resource('books', BookController::class);
+
+// Rotas para Authors
+Route::resource('authors', AuthorController::class);
+
+// Rotas para Categories
+Route::resource('categories', CategoryController::class);
+
+// Rotas para Publishers
+Route::resource('publishers', PublisherController::class);
