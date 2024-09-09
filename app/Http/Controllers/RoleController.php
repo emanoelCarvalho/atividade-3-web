008e93d5f2df44e299e4af2c2327b57410524285
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2024-09-09 08:17:57 -0300

    :sparkles: feat: Add update functionality to RoleController

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
-use App\Models\User; 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 
 
-class RoleController extends Controller 
+class RoleController extends Controller
 {
-    public function index() 
+    public function index()
     {
         Gate::authorize('admin', User::class);
 
@@ -17,10 +17,23 @@ class RoleController extends Controller
         return view('roles.index', compact('users'));
     }
 
-    public function edit(User $user) 
+    public function edit(User $user)
+    {
+        Gate::authorize('admin', User::class);
+        $user =  User::findOrFail($user->id);
+        return view('roles.edit', compact('user'));
+    }
+
+    public function update(User $user)
     {
         Gate::authorize('admin', User::class);
 
-        return view('roles.edit', compact('user'));
-    } 
+        $validatedData = request()->validate([
+            'role' => 'required|string|in:admin,librarian,cliente',
+        ]);
+
+        $user->update($validatedData);
+
+        return redirect()->route('roles.index')->with('success', 'Função atualizada com sucesso!');
+    }
 }
---
+++ resources/views/roles/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Lista de Usuários</h1>
+        <a href="{{ route('publishers.create') }}" class="btn btn-primary mb-3">Adicionar Nova Editora</a>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>roles</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->address }}</td>
+                        <td>
+                            <a href="{{ route('roles.edit', $user->id) }}" class="btn btn-warning">Editar</a>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
